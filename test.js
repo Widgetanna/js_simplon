67290e66a6e496ddba3d6efe7f84c40017bb1a4f
Lileczka <liliana.guderska@gmail.com>
2024-03-12 14:06:59 +0100

    full version

--- test.js
+++ test.js
@@ -1,7 +1,19 @@
-function trouverMaximum(tableau) {
-    return Math.max(...tableau);
-  }
-  
-  const numbers =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 100];
-  const resultat = trouverMaximum(numbers);
-  console.log("Le plus grand nombre est :", resultat);
+function trouverMotLePlusLong(phrase) {
+    const mots = phrase.split(' ');
+    let motLePlusLong = mots[0];
+
+    for (let i = 1; i < mots.length; i++) {
+        if (mots[i].length > motLePlusLong.length) {
+            motLePlusLong = mots[i];
+        }
+    }
+
+    return motLePlusLong;
+}
+
+const phrase = "La révolution numérique a transformé l'informatique en un champ infiniment vaste, explorant des concepts tels que l'intelligence artificielle, l'apprentissage automatique, la cybersécurité et la réalité virtuelle.";
+
+
+const resultatMotLePlusLong = trouverMotLePlusLong(phrase);
+
+console.log("Le mot le plus long dans la phrase :", resultatMotLePlusLong);
