c9e79f8685fd87326cccaebda8eb8e755d3ee87e
Lileczka <liliana.guderska@gmail.com>
2024-03-12 13:48:25 +0100

    avec spread operateur et méthode meth.max

--- test.js
+++ test.js
@@ -1,19 +1,7 @@
 function trouverMaximum(tableau) {
-   
-    
-    let maximum = tableau[0];
-
-  for (let i = 1; i < tableau.length; i++) {
-    if (tableau[i] > maximum) {
-      maximum = tableau[i];
-    }
-  }
-
-  return maximum;
-    
+    return Math.max(...tableau);
   }
   
-  
-const numbers =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 100];
-const resultat = trouverMaximum(numbers);
-console.log("Le plus grand nombre dans le tableau est :", resultat);
+  const numbers =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 100];
+  const resultat = trouverMaximum(numbers);
+  console.log("Le plus grand nombre est :", resultat);
